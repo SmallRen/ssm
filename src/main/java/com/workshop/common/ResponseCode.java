fdbc7633cd1c655efa98b7634f9df5c9e4438a1a
luoxinmie <3133778732@qq.com>
2019-05-22 22:23:06 +0800

    init

---
+++ src/main/java/com/workshop/common/ResponseCode.java
@@ -0,0 +1,15 @@
+package com.workshop.common;
+
+
+public enum ResponseCode {
+    OK(1, "操作成功"),
+    FAIL(-1, "操作失败");
+    public Integer code;
+    public String msg;
+
+    ResponseCode(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+}
+
